72a023501b6b5f47bd0800a818ed9594c951158c
nitin_45 <2005466@kiit.ac.in>
2023-04-11 23:59:40 +0530

    Intro page done

--- lib/app/modules/intro/views/intro_view.dart
+++ lib/app/modules/intro/views/intro_view.dart
@@ -9,14 +9,95 @@ class IntroView extends GetView<IntroController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('IntroView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'IntroView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                flex: 3,
+                child: Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius:
+                          BorderRadius.only(bottomRight: Radius.circular(50))),
+                  child: Column(
+                    children: [
+                      Expanded(child: Image.asset("assets/images/Intro.png"))
+                    ],
+                  ),
+                )),
+            Expanded(
+                flex: 2,
+                child: Container(
+                  color:Colors.blue,
+                  child: Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(50),
+                        )),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        children: [
+                          const Spacer(),
+                          const Text(
+                            "Welcome",
+                            style: TextStyle(
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const Spacer(),
+                          const Text(
+                            "Hello KIITIANS !",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                              wordSpacing: 2.5,
+                               fontWeight: FontWeight.bold,
+                              height: 1.5,
+                              fontSize: 16,
+                            ),
+                          ),
+                          const Spacer(
+                            flex: 3,
+                          ),
+                          //  Here we repleace sizebox with spacer
+                          Row(
+                            //button position
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              MaterialButton(
+                                height: 60,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15.0)),
+                                color: const Color.fromARGB(255, 226, 82, 82),
+                                onPressed: () {
+                                  //home screen path
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => const IntroView()));
+                                },
+                                child: const Text(
+                                  "Get Started",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                      color: Colors.white),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ))
+          ],
         ),
       ),
     );
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -10,7 +10,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.INTRO;
 
   static final routes = [
     GetPage(
