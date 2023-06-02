11f66e3d7ca68510a0c845b7d95464451979eae1
nitin_45 <nitinkumargd@gmail.com>
2023-06-02 15:17:54 +0530

    Resource page

--- lib/app/modules/home/views/pages/resources.dart
+++ lib/app/modules/home/views/pages/resources.dart
@@ -10,7 +10,7 @@ import '../../controllers/home_controller.dart';
 import '../widgets/hover_builder.dart';
 
 class ResourcesBody extends StatelessWidget {
-  const ResourcesBody(this.controller, {Key? key, required}) : super(key: key);
+  const ResourcesBody(this.controller, {Key? key}) : super(key: key);
   final HomeController controller;
 
   Future<void> openDoc(BuildContext context, IndexFile file) async {
@@ -41,9 +41,17 @@ class ResourcesBody extends StatelessWidget {
                         return const Center(child: Text('No files found'));
                       } else {
                         final list = controller.resController.currentEntity;
-                        list.removeWhere((element) =>
-                            element is IndexFile && element.size == 0);
-                        return resoursesListBuilder(list);
+                        list.removeWhere(
+                            (element) => element is IndexFile && element.size == 0);
+                        return LayoutBuilder(
+                          builder: (context, constraints) {
+                            if (constraints.maxWidth > 600) {
+                              return resoursesListBuilder(list, true);
+                            } else {
+                              return resoursesListBuilder(list, false);
+                            }
+                          },
+                        );
                       }
                     },
                   )
@@ -52,104 +60,131 @@ class ResourcesBody extends StatelessWidget {
         ),
       );
 
-  AnimationLimiter resoursesListBuilder(List<IndexEntity> list) =>
+  AnimationLimiter resoursesListBuilder(List<IndexEntity> list, bool isDesktop) =>
       AnimationLimiter(
         key: UniqueKey(),
         child: GetBuilder<HomeController>(
-            builder: (controller) => ListView.builder(
-                padding: GetPlatform.isWeb
-                    ? const EdgeInsets.only(right: 15, top: 5, bottom: 5)
-                    : const EdgeInsets.only(top: 5, bottom: 5),
-                physics: const BouncingScrollPhysics(),
-                itemCount: list.length,
-                itemBuilder: (context, index) =>
-                    AnimationConfiguration.staggeredList(
-                      duration: const Duration(milliseconds: 300),
-                      position: index,
-                      child: SlideAnimation(
-                        child: FadeInAnimation(
-                          child: GetPlatform.isWeb
-                              ? HoverBuilder(
-                                  builder: (context, isHovering) => entityTile(
-                                      list[index], context, isHovering))
-                              : entityTile(list[index], context, true),
-                        ),
-                      ),
-                    ))),
+          builder: (controller) => ListView.builder(
+            padding: isDesktop
+                ? const EdgeInsets.only(right: 15, top: 5, bottom: 5)
+                : const EdgeInsets.only(top: 5, bottom: 5),
+            physics: const BouncingScrollPhysics(),
+            itemCount: list.length,
+            itemBuilder: (context, index) => AnimationConfiguration.staggeredList(
+              duration: const Duration(milliseconds: 300),
+              position: index,
+              child: SlideAnimation(
+                child: FadeInAnimation(
+                  child: isDesktop
+                      ? HoverBuilder(
+                          builder: (context, isHovering) =>
+                              entityTile(list[index], context, isHovering),
+                        )
+                      : entityTile(list[index], context, true),
+                ),
+              ),
+            ),
+          ),
+        ),
       );
 
-  ListTile entityTile(
-          IndexEntity entity, BuildContext context, bool isHovering) =>
-      ListTile(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+  Widget entityTile(IndexEntity entity, BuildContext context, bool isHovering) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: InkWell(
         onTap: () => (entity is IndexFolder)
             ? controller.resController.currentPath = "${entity.path}/"
             : openDoc(context, entity as IndexFile),
-        leading: leadingIcon(
-          context,
-          isfolder: entity is IndexFolder,
-          fileName: entity.name,
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: const Offset(0, 3),
+              ),
+            ],
+            image: DecorationImage(
+              image: AssetImage('assets/images/backg.jpg'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.5), BlendMode.darken),
+            ),
+          ),
+          child: Row(
+            children: [
+              leadingIcon(
+                context,
+                isfolder: entity is IndexFolder,
+                fileName: entity.name,
+              ),
+              const SizedBox(width: 16),
+              Expanded(child: Text(entity.name)),
+              tileTrailing(isHovering, entity),
+            ],
+          ),
         ),
-        title: Text(entity.name),
-        trailing: tileTrailing(isHovering, entity),
-      );
+      ),
+    );
+  }
 
-  Widget? tileTrailing(bool isHovering, IndexEntity e) {
+  Widget tileTrailing(bool isHovering, IndexEntity e) {
     final isFav = controller.boxService.favBox.isFav(e.path);
     return Wrap(
       alignment: WrapAlignment.center,
       spacing: 20,
       children: [
-        e is IndexFile
-            ? isFav
-                ? glowingFavIcon(e, isFav)
-                : AnimatedOpacity(
-                    opacity: isHovering ? 1 : 0,
-                    curve: Curves.easeIn,
-                    duration: const Duration(milliseconds: 500),
-                    child: isHovering
-                        ? glowingFavIcon(e, isFav)
-                        : const SizedBox(),
-                  )
-            : const SizedBox(),
-        (e is IndexFolder
-                ? null
-                : Text(controller.resController
-                    .kiloBytesToString((e as IndexFile).size))) ??
-            const SizedBox(),
+        if (e is IndexFile)
+          isFav
+              ? glowingFavIcon(e, isFav)
+              : AnimatedOpacity(
+                  opacity: isHovering ? 1 : 0,
+                  curve: Curves.easeIn,
+                  duration: const Duration(milliseconds: 500),
+                  child: isHovering ? glowingFavIcon(e, isFav) : const SizedBox(),
+                ),
+        if (e is IndexFile)
+          Text(controller.resController.kiloBytesToString((e as IndexFile).size)),
       ],
     );
   }
 
   Widget glowingFavIcon(IndexFile e, bool isFav) => GestureDetector(
-      onTap: () async {
-        isFav
-            ? await controller.boxService.favBox.removeFav(e.path)
-            : await controller.boxService.favBox
-                .saveFav(DateWrapper(date: DateTime.now(), file: e).toJson());
-        controller.rerender();
-      },
-      child: isFav
-          ? const FaIcon(FontAwesomeIcons.solidStar, color: Colors.yellow)
-          : const FaIcon(FontAwesomeIcons.star));
+        onTap: () async {
+          isFav
+              ? await controller.boxService.favBox.removeFav(e.path)
+              : await controller.boxService.favBox
+                  .saveFav(DateWrapper(date: DateTime.now(), file: e).toJson());
+          controller.rerender();
+        },
+        child: isFav
+            ? const FaIcon(FontAwesomeIcons.solidStar, color: Colors.yellow)
+            : const FaIcon(FontAwesomeIcons.star),
+      );
 
   Widget leadingIcon(BuildContext context,
       {required bool isfolder, String fileName = ''}) {
     if (isfolder) {
       return Theme(
         data: Theme.of(context),
-        child: FaIcon(FontAwesomeIcons.folder,
-            color: context.isDarkMode
-                ? const Color(0xff0072B5)
-                : Get.theme.primaryColor),
+        child: FaIcon(
+          FontAwesomeIcons.folderClosed,
+          color: context.isDarkMode
+              ? const Color(0xff0072B5)
+              : Get.theme.primaryColor,
+        ),
       );
     } else {
       final fileName0 = fileName.toLowerCase();
       if (fileName0.endsWith('.pdf')) {
         return FaIcon(FontAwesomeIcons.filePdf, color: Colors.red[400]);
       } else if (fileName0.endsWith('.pptx') || fileName0.endsWith('.ppt')) {
-        return FaIcon(FontAwesomeIcons.filePowerpoint,
-            color: Colors.yellow[400]);
+        return FaIcon(FontAwesomeIcons.filePowerpoint, color: Colors.yellow[400]);
       } else if (fileName0.endsWith('.docx') || fileName0.endsWith('.doc')) {
         return const FaIcon(FontAwesomeIcons.fileWord, color: Colors.blue);
       } else if (fileName0.endsWith('.xlsx') || fileName0.endsWith('.xls')) {
@@ -159,7 +194,16 @@ class ResourcesBody extends StatelessWidget {
           fileName0.endsWith('.jpeg')) {
         return const FaIcon(FontAwesomeIcons.fileImage, color: Colors.purple);
       }
-      return const FaIcon(FontAwesomeIcons.solidFile);
+      return const FaIcon(FontAwesomeIcons.solidFolderClosed);
+    }
+  }
+
+  Future<void> launchUrl(Uri uri) async {
+    if (await canLaunch(uri.toString())) {
+      await launch(uri.toString());
+    } else {
+      throw 'Could not launch $uri';
     }
   }
 }
+
